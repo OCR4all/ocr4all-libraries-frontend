f6c18b71dcb733d07d8e8694a372ab7e395ed535
maxnth <maximilian.noeth@protonmail.com>
2024-01-23 21:06:32 +0100

    update topbar

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Bars3Icon } from "@heroicons/vue/24/outline";
+import { Bars3CenterLeftIcon } from "@heroicons/vue/24/outline";
 import { useAuthStore } from "@/stores/auth.store";
 const authStore = useAuthStore();
 </script>
@@ -9,25 +9,23 @@ const authStore = useAuthStore();
     class="flex h-16 justify-between space-x-6 border-b border-gray-200 bg-white px-3 py-3 dark:border-zinc-700 dark:bg-zinc-800 md:space-x-6 md:px-6 z-20 shadow-md"
   >
     <div class="ml-2 flex flex-1">
-      <img src="/img/logo.svg" class="h-10 w-10" alt="OCR4all logo" />
       <button
         @click="$emit('toggle-sidebar-lg', false)"
-        class="ml-40 mt-2 flex hidden h-6 w-6 flex-shrink-0 items-center justify-center rounded-full text-gray-600 hover:ring-2 hover:ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-600 dark:text-white lg:block"
+        class="mt-1 text-gray-600 rounded-lg cursor-pointer hidden lg:block hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
         type="button"
         value="Open sidebar"
       >
-        <Bars3Icon class="h-6 w-6" />
+        <Bars3CenterLeftIcon class="h-8 w-8" />
       </button>
-    </div>
-    <div class="flex flex-1 items-center">
       <button
-        @click="$emit('toggle-sidebar-md', true)"
-        class="flex h-6 w-6 flex-shrink-0 items-center justify-center rounded-full text-gray-600 hover:ring-2 hover:ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-600 dark:text-white lg:hidden"
+        @click="$emit('toggle-sidebar-md', false)"
+        class="mt-1 text-gray-600 rounded-lg cursor-pointer block lg:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
         type="button"
         value="Open sidebar"
       >
-        <Bars3Icon class="h-6 w-6" />
+        <Bars3CenterLeftIcon class="h-8 w-8" />
       </button>
+      <img src="/img/logo.svg" class="ml-8 h-10 w-10" alt="OCR4all logo" />
     </div>
     <DarkModeToggle />
     <div v-show="authStore.user">
