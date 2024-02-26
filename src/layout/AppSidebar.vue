57b7b9e9a700c7bd3464509efb7fa48344da333f
maxnth <maximilian.noeth@protonmail.com>
2024-02-26 14:06:26 +0100

    update project

--- src/layout/AppSidebar.vue
+++ src/layout/AppSidebar.vue
@@ -115,6 +115,42 @@ defineExpose({
                 />
                 {{ $t(item.label) }}
               </router-link>
+              <div
+                v-show="uiStore.sidebarCtaVisible"
+                id="dropdown-cta"
+                class="mt-6 rounded-md bg-primary-50 p-4 dark:bg-primary-900"
+                role="alert"
+              >
+                <div class="mb-3 flex items-center">
+            <span
+              class="mr-2 bg-orange-100 px-2.5 py-0.5 text-sm font-semibold text-orange-800 dark:bg-orange-200 dark:text-orange-900"
+            >{{ $t("nav.sidebar.cta.header") }}</span
+            >
+                  <button
+                    type="button"
+                    class="-mx-1.5 -my-1.5 ml-auto inline-flex h-6 w-6 bg-primary-50 p-1 text-primary-900 hover:bg-primary-200 focus:ring-2 focus:ring-primary-400 dark:bg-primary-900 dark:text-primary-200 dark:hover:bg-primary-800"
+                    @click="uiStore.sidebarCtaVisible = false"
+                  >
+                    <span class="sr-only">Close</span>
+                    <svg
+                      aria-hidden="true"
+                      class="h-4 w-4"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+                <p class="mb-3 text-sm text-blue-800 dark:text-blue-400">
+                  {{ $t("nav.sidebar.cta.content") }}
+                </p>
+              </div>
             </div>
           </div>
         </div>
