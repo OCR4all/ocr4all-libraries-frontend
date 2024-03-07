d6e9795463a6e8e5562877040e67533d5fcd1287
maxnth <maximilian.noeth@protonmail.com>
2024-03-07 11:58:19 +0100

    update project

--- src/components/NodeFlow/Custom/CToolbar.vue
+++ src/components/NodeFlow/Custom/CToolbar.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { Commands } from "@baklavajs/renderer-vue";
 import { useViewModel } from "baklavajs";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n()
 
 const { viewModel } = useViewModel();
 
@@ -13,7 +16,7 @@ const emit = defineEmits(["new", "load", "save", "zoom-in", "zoom-out", "rate-wo
   >
     <div class="flex justify-between">
       <div class="flex space-x-4">
-        <span v-tooltip="'Undo'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.undo')">
         <i
           class="pi pi-undo self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="
@@ -21,7 +24,7 @@ const emit = defineEmits(["new", "load", "save", "zoom-in", "zoom-out", "rate-wo
           "
         ></i>
       </span>
-        <span v-tooltip="'Redo'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.redo')">
         <i
           class="pi pi-undo -scale-x-100 self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="
@@ -32,19 +35,19 @@ const emit = defineEmits(["new", "load", "save", "zoom-in", "zoom-out", "rate-wo
         <div
           class="inline-block w-0.5 self-stretch bg-surface-500 opacity-100 dark:bg-surface-400 dark:opacity-50"
         ></div>
-        <span v-tooltip="'New Workflow'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.new-workflow')">
         <i
           class="pi pi-plus self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="emit('new')"
         ></i>
       </span>
-        <span v-tooltip="'Load Workflow'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.load-workflow')">
         <i
           class="pi pi-upload self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="emit('load')"
         ></i>
       </span>
-        <span v-tooltip="'Save Workflow'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.save-workflow')">
         <i
           class="pi pi-save self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="emit('save')"
@@ -53,19 +56,19 @@ const emit = defineEmits(["new", "load", "save", "zoom-in", "zoom-out", "rate-wo
         <div
           class="inline-block w-0.5 self-stretch bg-surface-500 opacity-100 dark:bg-surface-400 dark:opacity-50"
         ></div>
-        <span v-tooltip="'Select'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.select')">
         <i
           class="pi pi-copy self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="viewModel.commandHandler.executeCommand(Commands.START_SELECTION_BOX_COMMAND)"
         ></i>
       </span>
-        <span v-tooltip="'Copy'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.copy')">
         <i
           class="pi pi-copy self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="viewModel.commandHandler.executeCommand(Commands.COPY_COMMAND)"
         ></i>
       </span>
-        <span v-tooltip="'Paste'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.paste')">
         <i
           class="pi pi-search-plus self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="viewModel.commandHandler.executeCommand(Commands.PASTE_COMMAND)"
@@ -74,13 +77,13 @@ const emit = defineEmits(["new", "load", "save", "zoom-in", "zoom-out", "rate-wo
         <div
           class="inline-block w-0.5 self-stretch bg-surface-500 opacity-100 dark:bg-surface-400 dark:opacity-50"
         ></div>
-        <span v-tooltip="'Zoom Out'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.zoom-out')">
         <i
           class="pi pi-search-minus self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="emit('zoom-out')"
         ></i>
       </span>
-        <span v-tooltip="'Zoom In'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.zoom-in')">
         <i
           class="pi pi-search-plus self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="emit('zoom-in')"
@@ -89,7 +92,7 @@ const emit = defineEmits(["new", "load", "save", "zoom-in", "zoom-out", "rate-wo
         <div
           class="inline-block w-0.5 self-stretch bg-surface-500 opacity-100 dark:bg-surface-400 dark:opacity-50"
         ></div>
-        <span v-tooltip="'Rate Workflow'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.rate-workflow')">
         <i
           class="pi pi-check-circle self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="emit('rate-workflow')"
@@ -98,7 +101,7 @@ const emit = defineEmits(["new", "load", "save", "zoom-in", "zoom-out", "rate-wo
         <div
           class="inline-block w-0.5 self-stretch bg-surface-500 opacity-100 dark:bg-surface-400 dark:opacity-50"
         ></div>
-        <span v-tooltip="'Toggle Palette'">
+        <span v-tooltip="t('pages.nodeflow.toolbar.tooltips.toggle-palette')">
         <i
           class="pi pi-align-right self-center text-xl text-surface-600 hover:cursor-pointer hover:text-black dark:text-surface-400 dark:hover:text-white"
           @click="emit('toggle-palette')"
