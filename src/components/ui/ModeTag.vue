db253edcde823aeda5c72415b1ddb4575109474d
maxnth <maximilian.noeth@protonmail.com>
2024-07-09 10:02:30 +0200

    adjust mode tag for mobile layout

--- src/components/ui/ModeTag.vue
+++ src/components/ui/ModeTag.vue
@@ -2,11 +2,11 @@
 import IconTestTube from '~icons/mdi/test-tube'
 </script>
 <template>
-  <span
+  <div
     class="flex gap-x-2 align-center rounded bg-blue-100 px-2.5 py-2 text-sm font-medium text-surface-700 dark:bg-blue-900 dark:text-surface-100"
     >
     <IconTestTube class="self-center" />
-    <p class="self-center">Experimental Mode</p>
-  </span
+    <p class="hidden lg:block self-center">Experimental Mode</p>
+  </div
   >
 </template>
