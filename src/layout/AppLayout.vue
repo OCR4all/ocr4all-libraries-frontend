293893a36d86506b5dfdfbf10b0c7282c7df1a0a
maxnth <maximilian.noeth@protonmail.com>
2024-07-10 11:30:43 +0200

    update layout

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -30,7 +30,7 @@ function toggleSidebarMobile() {
       <AppSidebar :mode="SidebarMode.Main" />
       <AppSidebarMobile :mode="SidebarMode.Main" ref="mobileSidebarRef" />
       <main
-        class="flex-1 justify-center overflow-x-auto bg-surface-100 shadow-sm dark:bg-surface-950 lg:m-2 lg:rounded-xl lg:bg-surface-0 lg:dark:bg-surface-900"
+        class="flex-1 justify-center overflow-x-auto bg-surface-100 shadow-md dark:bg-surface-950 lg:m-1.5 lg:rounded-md lg:bg-surface-0 lg:dark:bg-surface-900"
       >
         <AppTopbar
           @toggle-sidebar-mobile="toggleSidebarMobile" />
