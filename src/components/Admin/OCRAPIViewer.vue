d31539600f596ba0003bd4d9a7150eb40e4cec4d
maxnth <maximilian.noeth@protonmail.com>
2024-09-08 19:26:38 +0200

    add placeholder for ocr-d api viewer

--- src/components/Admin/OCRAPIViewer.vue
+++ src/components/Admin/OCRAPIViewer.vue
@@ -1,2 +1,24 @@
-<script setup lang="ts"></script>
-<template></template>
+<script setup lang="ts">
+import AppLayout from "@/layout/AppLayout.vue";
+
+useHead({
+  title: "OCR-D API Viewer",
+  titleTemplate: "%s %separator %siteName",
+  templateParams: { separator: "|", siteName: "OCR4all" },
+  bodyAttrs: { class: { overflow: true } },
+});
+</script>
+
+<template>
+    <div
+        class="flex h-full flex-col items-center justify-center px-6"
+    >
+      <div class="mx-auto text-center">
+        <h1
+            class="mb-4 text-4xl font-extrabold tracking-tight text-black dark:text-white"
+        >
+          Please establish a tunnel to the OCR-D API
+        </h1>
+      </div>
+    </div>
+</template>
